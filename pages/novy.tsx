9575172410586fb483c22079b816a7100f7545a9
lukascobit <lukyn.odehnal333@gmail.com>
2023-02-10 22:28:36 +0100

    button styling

--- pages/novy.tsx
+++ pages/novy.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Meta from "../components/Meta";
+import Nav from "../components/Nav";
 
 function Novy() {
 	return (
 		<div className="content">
+			<Nav />
+			<Meta title="Swipechoose" />
 			<h1>post new </h1>
+			<input type="file" />
+			<button>post</button>
 		</div>
 	);
 }
