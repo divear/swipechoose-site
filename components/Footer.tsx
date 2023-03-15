324b6064dbf1a57238ccc447e2bb1fb1e511e93a
lukascobit <lukyn.odehnal333@gmail.com>
2023-03-15 16:08:24 +0100

    signoff working

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleAuthProvider, signOut, auth } from "../components/firebase";
 
 function Footer() {
+	function signoff() {
+		// const provider = new GoogleAuthProvider();
+		signOut(auth);
+
+		localStorage.clear();
+		window.location.reload();
+	}
 	return (
 		<div className="footer">
-			<button className="signoff">sign off</button>
+			<button onClick={signoff} className="signoff">
+				sign off
+			</button>
 			<p className="footerText">divear 2023</p>
 		</div>
 	);
